6a5d9d581e03be10fcaaa47c41855e568ac67c3c
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-02-13 12:45:09 -0300

    correções feitas

--- main1.js
+++ main1.js
@@ -1,36 +1,37 @@
 const form = document.getElementById('numeros');
 const numA = document.getElementById('CampoA');
 const numB= document.getElementById('CampoB');
-let formEValido = false;
 
-function ValidarNum (numA, numB){
-    numA = parseFloat (CampoA);
-    numB = parseFloat (CampoB);
-    numB > numA
-        
+function ValidarNum (nA, nB){
+    //numA = parseFloat (CampoA);
+    //numB = parseFloat (CampoB);
+    //numB > numA
+      const validacao =   nA < nB
+      return validacao;
     }
 
 form.addEventListener('submit', function(e){
-
     e.preventDefault();
-    const numA = document.getElementById('CampoA');
-    const numB = document.getElementById('CampoB');
-    const MensagemSucesso = `Sucesso! o montante de: ${CampoB.value}</b> é maior que: ${CampoA.value}`;
+
+    let formEValido = false;
+
+    //const numA = document.getElementById('CampoA');
+    //const numB = document.getElementById('CampoB');
+    const MensagemSucesso = `Sucesso! o montante de: ${numB.value}</b> É maior que: ${numA.value}`;
     
 
-    formEValido = ValidarNum(CampoA.value, CampoB.value) 
+    formEValido = ValidarNum(numA.value, numB.value) 
     if (formEValido){
         const containerMensagemSucesso = document.querySelector(".sucess");
         containerMensagemSucesso.innerHTML = MensagemSucesso;
         containerMensagemSucesso.style.display = 'block';      
 
-        CampoA.value = '';
-        CampoB.value = '';
+        //CampoA.value = '';
+        //CampoB.value = '';
 
         } else{
             CampoB.style.border = '1px solid red';
             document.querySelector('.error').style.display = 'block'; 
         }
     
-    });
-
+    })
