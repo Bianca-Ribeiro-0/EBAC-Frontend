35315655697758dbbd8d82bb92b008cbe10da156
Bianca Ribeiro <103955288+Bianca-Ribeiro-0@users.noreply.github.com>
2023-04-20 12:56:42 -0300

    Update main1.js

--- main1.js
+++ main1.js
@@ -3,9 +3,6 @@ const numA = document.getElementById('CampoA');
 const numB= document.getElementById('CampoB');
 
 function ValidarNum (nA, nB){
-    //numA = parseFloat (CampoA);
-    //numB = parseFloat (CampoB);
-    //numB > numA
       const validacao =   nA < nB
       return validacao;
     }
@@ -14,9 +11,6 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
 
     let formEValido = false;
-
-    //const numA = document.getElementById('CampoA');
-    //const numB = document.getElementById('CampoB');
     const MensagemSucesso = `Sucesso! o montante de: ${numB.value}</b> É maior que: ${numA.value}`;
     
 
@@ -25,10 +19,6 @@ form.addEventListener('submit', function(e){
         const containerMensagemSucesso = document.querySelector(".sucess");
         containerMensagemSucesso.innerHTML = MensagemSucesso;
         containerMensagemSucesso.style.display = 'block';      
-
-        //CampoA.value = '';
-        //CampoB.value = '';
-
         } else{
             CampoB.style.border = '1px solid red';
             document.querySelector('.error').style.display = 'block'; 
