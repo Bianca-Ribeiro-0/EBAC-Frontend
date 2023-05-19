f3d6196e693fa5a209b89cc7293c8b7c94f27940
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-05-18 22:35:57 -0300

    sorteador de numeros com grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt){
         less: {
             development: {
                 files: {
-                    'main.css' : 'main.less'
+                    'dev/styles/main.css' : 'src/styles/main.less'
                 }
             },
             production: {
@@ -12,34 +12,95 @@ module.exports = function(grunt){
                     compress: true,
                 },
                 files:{
-                    'main.min.css' : 'main.less'
+                    'dist/styles/main.min.css' : 'src/styles/main.less'
                 }
             }
         },
-        sass: {
-            dist:{
+        watch:{
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+        replace:{
+            dev: {
                 options:{
-                    style: ' compressed'
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS', 
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS', 
+                            replacement: './src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options:{
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS', 
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS', 
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            },
+        },
+        htmlmin: {
+            dist: {
+                options:{
+                    removeComments: true,
+                    collapseWhitespace: true,
                 },
                 files: {
-                    'main.css' : 'main.scss'
+                    'prebuild/index.html': 'src/index.html'
                 }
             }
         },
-        concurrent: {
-            target: ['ola','less','sass']
+        clean: ['prebuild'],
+        uglify : {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                }
+            }
         }
     })
 
-    grunt.registerTask('ola', function() {
-        const done = this.async();
-        setTimeout(function() {
-         console.log('olá Grunt');
-         done();
-        }, 3000);   
-    })
+
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-concurrent');
-    grunt.registerTask('default', ['concurrent']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
