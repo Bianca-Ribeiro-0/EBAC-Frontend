415cafc150a1fd1419767ee81238dca44890347f
Bianca Ribeiro <103955288+Bianca-Ribeiro-0@users.noreply.github.com>
2023-06-29 19:32:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,22 +3,27 @@ function Pessoa(nome) {
     this.dizOi = function(){
         console.log(this.nome + " diz Olá");
     }
-}
+}  //clase pessoa
 
+//classe funcionario possui 3 instancias
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
-    // this.salario = salario;
-    let _salario = salario;
+    let _salario = salario;            
 
     //get e set - recupera e seta valores
     this.getSalario = function() {
-        return `O salário de ${this.nome} é ${_salario}`;
+        return _salario;
     }
 
     this.setSalario = function(valor) {
         if (typeof valor === 'number') {
             _salario = valor;
         }
+    }
+
+    this.aumento = function() {
+        const novoSalario = _salario * 1.1;
+        _salario = novoSalario;
     }
 
     this.dizCargo = function() {
@@ -28,12 +33,36 @@ function Funcionario(nome, cargo, salario) {
     Pessoa.call(this, nome);
 }
 
-const pessoa1 = new Pessoa("Maria");
+function Estagiario(nome) {
+    Funcionario.call(this, nome, "Estagiario", 2000);
+    
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.07;
+        this.setSalario(novoSalario);
+        _salario = novoSalario;
+    }
+}
+
+function Gerente(nome) {
+    Funcionario.call(this, nome, "Gerente", 10000);
+    
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.15;
+        this.setSalario(novoSalario);
+        _salario = novoSalario;
+    }
+}
+
+
 const funcionario1 = new Funcionario("Maria", "dev front-end", 5000);
+const funcionario2 = new Estagiario("Pedro");
+const funcionario3 = new Gerente("Amanda");
 
-funcionario1.dizOi();
-funcionario1.dizCargo();
+funcionario1.aumento();
+console.log(funcionario1.getSalario());
 
-funcionario1.setSalario(7000);
+funcionario2.aumento();
+console.log(funcionario2.getSalario());
 
-console.log(funcionario1.getSalario())
+funcionario3.aumento();
+console.log(funcionario3.getSalario());
